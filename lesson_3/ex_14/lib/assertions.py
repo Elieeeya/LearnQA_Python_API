1b54ee7975c1f1534c93704481a8b6c11e1ae6f4
Elieeeya <ik.slydnev@gmail.com>
2023-10-28 19:55:05 +0300

    ДЗ по Теме 4, задание: ex_16

--- lesson_3/ex_14/lib/assertions.py
+++ lesson_3/ex_14/lib/assertions.py
@@ -11,3 +11,27 @@ class Assertions:
             assert False, f"Response is not in JSON format. Response text is {response.text}"
         assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
         assert response_as_dict[name] == expected_value, f"{error_message}. Expected value is {expected_value}, but actual value is {response_as_dict[name]}"
+
+    @staticmethod
+    def assert_json_has_key(response: Response, name):
+        response_as_dict = response.json()
+        assert name in response_as_dict, f"Response JSON doesn't have key {name}"
+
+    @staticmethod
+    def assert_json_has_not_key(response: Response, name):
+        response_as_dict = response.json()
+        assert name not in response_as_dict, f"Response JSON shouldn't have key {name}. But it's present."
+
+    @staticmethod
+    def assert_code_status(response: Response, expected_status_code):
+        assert response.status_code == expected_status_code, f"Unexpected status code! Expected: {expected_status_code}, Actual: {response.status_code}"
+
+    @staticmethod
+    def assert_json_has_keys(response: Response, names: list):
+        response_as_dict = response.json()
+        for name in names:
+            assert name in response_as_dict, f"Response JSON doesn't have key {name}"
+
+    @staticmethod
+    def assert_text_message(response: Response, status_code):
+        assert status_code == 400, "Test didn't pass successfully"
---
+++ lesson_4/ex_16.py
@@ -0,0 +1,23 @@
+import requests
+from lesson_3.ex_14.lib.base_case import BaseCase
+from lesson_3.ex_14.lib.assertions import Assertions
+
+
+class TestUserGet(BaseCase):
+    def test_get_user_details_not_auth(self):
+        response = requests.get("https://playground.learnqa.ru/api/user/2")
+        Assertions.assert_json_has_key(response, "username")
+        for field in ["email", "firstName", "lastName"]:
+            Assertions.assert_json_has_not_key(response, field)
+
+
+    def test_get_user_details_auth_as_same_user(self):
+        auth_data = {"email": "vinkotov@example.com", "password": "1234"}
+        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=auth_data)
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+        user_id = self.get_json_value(response1, "user_id")
+        response2 = requests.get(f"https://playground.learnqa.ru/api/user/{user_id}",
+                                 headers={"x-csrf-token": token},
+                                 cookies={"auth_sid": auth_sid})
+        Assertions.assert_json_has_keys(response2, ["username", "email", "firstName", "lastName"])
